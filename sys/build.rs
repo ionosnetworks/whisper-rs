6e6d9225f3e04515bed227b826935d6c44e43ca8
Nimi Wariboko Jr <nimi.wariboko@livereachmedia.com>
2025-06-06 00:56:36 +0000

    Search stubs first

--- sys/build.rs
+++ sys/build.rs
@@ -59,10 +59,10 @@ fn main() {
                 println!("cargo:rustc-link-search={}", cuda_path.display());
             } else {
                 // println!("cargo:rustc-link-lib=dylib=culibos");
-                //println!("cargo:rustc-link-search=/usr/local/cuda/lib64");
                 println!("cargo:rustc-link-search=/usr/local/cuda/lib64/stubs");
-                //println!("cargo:rustc-link-search=/opt/cuda/lib64");
+                println!("cargo:rustc-link-search=/usr/local/cuda/lib64");
                 println!("cargo:rustc-link-search=/opt/cuda/lib64/stubs");
+                println!("cargo:rustc-link-search=/opt/cuda/lib64");
             }
         }
     }
